16011fda0b81563cda68edccfae5efdfed7c22d8
YURESH-SUBEDI <subediyuresh1640@gmail.com>
2024-08-06 16:16:15 +0545

    Add files via upload

---
+++ assinment1.py
@@ -0,0 +1,40 @@
+num=101
+if (num>101):
+    print("this is old men")
+elif(num> 40):   
+    print("youngest men")
+elif(num> 15): 
+    print("child")
+else:    
+    print("nothings")
+    #second
+num=90
+if (num>80):
+    print("this is old men")
+elif(num> 40):   
+    print(" this is youngest men")
+elif(num> 15): 
+    print("child")
+else:    
+    print("nothings")
+    # third
+num=40
+if (num>80):
+    print("this is old men")
+elif(num> 40):   
+    print(" this is youngest men")
+elif(num> 15): 
+    print("child")
+else:    
+    print("nothings")
+    # fourth
+num=15
+if (num>80):
+    print("this is old men")
+elif(num> 40):   
+    print(" this is youngest men")
+elif(num> 15): 
+    print("child")
+else:    
+    print("nothings")
+        
